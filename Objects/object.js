0a2bf16a5fbf3068f823c541b05b2c3246211b7c
shriramt124 <shriramt.124@gmail.com>
2023-09-09 15:59:26 +0530

    object-modified

--- Objects/object.js
+++ Objects/object.js
@@ -51,3 +51,114 @@ delete objectName.propertyName;
 
 
 */
+function emp(id,name,city){//ye function ek constructor ki tarah kaam kar raha hai
+    this.id = id;
+    this.name = name;
+    this.city = city;
+}
+const constobj = new emp(890,"shriram","idnore");//new keyword ka jadoo hai ki ye object bana deta hai
+console.log(constobj)
+const employee ={
+    name:"shriram",
+    age:'22',
+    salary:"0000",
+}
+console.log(employee)
+const keyvalues = Object.keys(employee)//return array of keys 
+console.log(keyvalues)
+const valuesofkey = Object.values(employee)//return an array of vlaues of keys
+console.log(valuesofkey)
+
+const keyvaluepairs = Object.entries(employee)//return an arrya of array 
+console.log(keyvaluepairs)//[ [ 'name', 'shriram' ], [ 'age', '22' ], [ 'salary', '0000' ] ]
+
+//if we again want to make object using keyvalue pairs then 
+const retobject = Object.fromEntries(keyvaluepairs)
+console.log(retobject)
+ 
+//object ko dursre variable me assign kar sakte hain
+const assignobject = Object.assign({},employee);//employe object empty object me copy hua aur wo assignobject me assign ho gaya
+
+console.log(assignobject)
+//ye jo assignobject and employee object hain bhale hi employee naam ke object ki values assign hui hai assignobject naam 
+//ke object me par dono hi memory me alag alag refer kar rahe ha
+//yani ki dono same nahi hain 
+console.log(assignobject == employee)//output false because dono alag memory occupy kar rahe hain
+
+//object ko freez karne ke baad ham usme kisi bhi value ko change or modify nahi kar sakte
+//dhyan rahe na hi ham kisi vlaue ko add kar sakte hain nahi modify kar sakte hain
+
+Object.freeze(employee);
+employee.name = "arjun"
+console.log(employee.name)//yahan name change nahi hua kynki ham object ko freeze kar chuke hain
+
+//check karsakte hain ki freeze hai ki nahi
+console.log(Object.isFrozen(employee))//agar freeze hoga to true aayega nahi to false aayega
+
+//ek seal method aur Object.isseal() method hoga 
+// Object.seal() karne se ham nayi propery add nahi kar sakte ham par existing propery ko modify kar sakte hain 
+var newemp = {
+    name:"ram",
+    age:'50',
+    salary:5000,
+}
+newemp.branch = "cse"//Object.seal() ke pahle nayi property add ho gayi
+console.log(newemp)//{ name: 'ram', age: '50', salary: 5000, branch: 'cse' }
+Object.seal(newemp);//seal kar diya 
+newemp.id = "780"
+console.log(newemp)//{ name: 'ram', age: '50', salary: 5000, branch: 'cse' }
+newemp.marks = 90//nayi propery add nahi hogi seal ke baad 
+newemp.name = "arjun"//existing propety modify ho jayegi seal ke baad
+console.log(newemp)//{ name: 'arjun', age: '50', salary: 5000, branch: 'cse' }
+
+console.log(Object.isSealed(newemp))//agar sealed hoga to true aayega
+
+
+ console.log(Object.hasOwn(employee,'name'))//check kiye ki kya employe naam ke object ke andar name key present hai ki nahi
+ console.log(Object.getOwnPropertyNames(employee))//emplyee object ke andar sare ownproperty ka array aa jayega
+ //[ 'name', 'age', 'salary' ]
+let descriptor =  console.log(Object.getOwnPropertyDescriptor(newemp,'name'))
+/*{
+    value: 'arjun',
+    writable: true,
+    enumerable: true,
+    configurable: false
+  }
+  */
+console.log(descriptor)
+
+//ab agar hame value ,writable,configurable ko change karna hai to ham Object.definePrroperty use karte hain
+const properytesting = {
+    name:"papa",
+    branch:"autoparts",
+}
+Object.defineProperty(properytesting,'name',{
+    value:'priyanka',
+    writable:false,
+    enumerable:true,
+    configurable:true
+})
+let newdescriptor = Object.getOwnPropertyDescriptor(properytesting,'name')
+console.log(newdescriptor)
+//agar do properties ke bare me detail dalna hai
+let data = {
+    name:"palher",
+    branch:"textile",
+}
+Object.defineProperties(data,{
+    name:{
+        value : "beta",
+        writable:false,
+        configurable:false,
+        enumerable:true,//enumerable means ye value mujhe loop ke through nahi milegi
+    },
+    branch:{
+        value:"cse",
+        writable:false,
+        enumerable:true,
+    }
+})
+ console.log(Object.keys(data))
+ let des = Object.getOwnPropertyDescriptor(data,"branch")
+ console.log(des);
+  
